e821a85c078cf135976165608db6b8417aa5053f
fizikistkomran <onmazutkuenes@gmail.com>
2025-09-01 10:09:37 +0300

    Add files via upload

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,6 @@
 # gunicorn.conf.py
 import os
 
-# Railway'in verdiği PORT'u oku; yoksa 8080
 bind = f"0.0.0.0:{os.getenv('PORT', '8080')}"
 workers = 1
 threads = 8
@@ -10,7 +9,7 @@ worker_class = "gthread"
 loglevel = "debug"
 
 # proxy header'ları (Railway için güvenli)
-forwarded_allow_ips = ["*"]
+forwarded_allow_ips = "*"
 secure_scheme_headers = {
     "X-FORWARDED-PROTOCOL": "ssl",
     "X-FORWARDED-PROTO": "https",
